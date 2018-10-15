15e476d482b129530f02a77ba828dafa23c2ce23
akshayneema <neemakshay111@gmail.com>
2018-10-15 22:43:14 +0530

    hitreward doublereward splitreward

--- blackjack.cpp
+++ blackjack.cpp
@@ -2,6 +2,9 @@
 #include <cstdlib>
 using namespace std;
 const int p;
+int freward[34][12]; //33x10 array which stores the final rewards
+int faction[34][12]; //33x10 array which stores the final actions
+
 struct State {
     // int pstateno; //ranges from 1 to 33 as per problem statement, 1-15 simple cases; 16-23 one card A another some simple card(not face); 24-33 pairs
     // int dealercard;// ranges from 2-11
@@ -13,21 +16,28 @@ struct State {
     int playercardlen;
     int dealerval;
     int dealersofta;
-    int dealercardlen; 
-    int deal;
-}
+    int dealercardlen;
+    bool pair;
+};
 //beware of soft A
 
-State* NumberToState(int pstateno, int dealercard)//underconstruction
+int statetonumber(State* s)//get your state number rn
 {
-    State* s;
-    s->pstateno=pstateno;
-    s->dealercard=dealercard;
-    if(pstateno>=1 && pstateno<=15)
+    if(s->playersofta==1)
     {
-        s->pvalue=pstateno+4;
-        s->playercards
+        if(s->playerval==12)//pair of ace--AA 33
+            return 33;
+        else//one Ace and other non-face card 16 to 23
+        {
+            return s->playerval+3;
+        }
     }
+    else if(s->pair==true)
+    {
+        return 22+s->playerval/2;//24 to 32
+    }
+    else
+        return s->playerval-4;//simple cases 1 to 15
 }
 
 void statecopy(State* tostate, State* fromstate)
@@ -38,34 +48,33 @@ void statecopy(State* tostate, State* fromstate)
     tostate->dealerval=fromstate->dealerval;
     tostate->dealersofta=fromstate->dealersofta;
     tostate->dealercardlen=fromstate->dealercardlen;
-    tostate->deal=fromstate->deal;
 }
 //NEED TO ADD BLACK-JACK FEATURE ALSO
-int afterstand(State* s)//expected reward after player stands. only dealer is making moves now.
+int standreward(State* s, int deal)//expected reward after player stands. only dealer is making moves now.
 {
     int reward;
     int rewardface;
     //reward to player face-card
     if(s->dealerval+10>21 && s->dealersofta==0)//dealer busts
-        rewardface=s->deal;
+        rewardface=deal;
     else if(s->dealerval+10>21 && s->dealersofta!=0)//dealer uses soft A
     {
         State* temps;
         statecopy(temps,s);
         temps->dealersofta=temps->dealersofta-1;
         temps->dealercardlen=temps->dealercardlen+1;
-        rewardface=afterstand(temps);
+        rewardface=standreward(temps,deal);
     }
     else if(s->dealerval+10<=21)
     {
         if(s->dealerval+10>=17)//stands(controversial)
         {
             if(s->dealerval+10>s->playerval)//player looses
-                rewardface=-1*s->deal;
+                rewardface=-1*deal;
             else if(s->dealerval+10==s->playerval)//PUSH
                 rewardface=0;
             else//player wins
-                rewardface=1*s->deal;
+                rewardface=1*deal;
         }
         else//dealer continues hitting
         {
@@ -73,7 +82,7 @@ int afterstand(State* s)//expected reward after player stands. only dealer is ma
             statecopy(temps,s);
             temps->dealerval=temps->dealerval+10;
             temps->dealercardlen=temps->dealercardlen+1;
-            rewardface=afterstand(temps);
+            rewardface=standreward(temps,deal);
         }
     }
     reward=p*rewardface;
@@ -83,7 +92,7 @@ int afterstand(State* s)//expected reward after player stands. only dealer is ma
     {
         int rewardnonface;
         if(s->dealerval+i>21 && s->dealersofta==0)//bust
-            rewardnonface=s->deal;
+            rewardnonface=deal;
         else if(s->dealerval+i>21 && s->dealersofta!=0)//use soft A
         {
             State* temps;
@@ -91,18 +100,18 @@ int afterstand(State* s)//expected reward after player stands. only dealer is ma
             temps->dealersofta=temps->dealersofta-1;
             temps->dealercardlen=temps->dealercardlen+1;
             temps->dealerval=temps->dealerval+i-10;
-            rewardnonface=afterstand(temps);
+            rewardnonface=standreward(temps,deal);
         }
         else if(s->dealerval+i<=21)
         {
             if(s->dealerval+i>=17)//stands(controversial)
             {
                 if(s->dealerval+i>s->playerval)//player looses
-                    rewardnonface=-1*s->deal;
+                    rewardnonface=-1*deal;
                 else if(s->dealerval+i==s->playerval)//PUSH
                     rewardnonface=0;
                 else//player wins
-                    rewardnonface=1*s->deal;
+                    rewardnonface=1*deal;
             }
             else//dealer continues hitting
             {
@@ -110,7 +119,7 @@ int afterstand(State* s)//expected reward after player stands. only dealer is ma
                 statecopy(temps,s);
                 temps->dealerval=temps->dealerval+i;
                 temps->dealercardlen=temps->dealercardlen+1;
-                rewardnonface=afterstand(temps);
+                rewardnonface=standreward(temps,deal);
             }
         }
         reward=reward+((1-p)/9)*rewardnonface;
@@ -122,11 +131,11 @@ int afterstand(State* s)//expected reward after player stands. only dealer is ma
         if(s->dealerval+1>=17)//stands(controversial)
         {
             if(s->dealerval+1>s->playerval)//player looses
-                rewardace=-1*s->deal;
+                rewardace=-1*deal;
             else if(s->dealerval+1==s->playerval)//PUSH
                 rewardace=0;
             else//player wins
-                rewardace=1*s->deal;   
+                rewardace=1*deal;   
         }
         else//dealer continues hitting
         {
@@ -134,7 +143,7 @@ int afterstand(State* s)//expected reward after player stands. only dealer is ma
             statecopy(temps,s);
             temps->dealerval=temps->dealerval+1;
             temps->dealercardlen=temps->dealercardlen+1;
-            rewardace=afterstand(temps);
+            rewardace=standreward(temps,deal);
         }
     }
     else
@@ -142,22 +151,264 @@ int afterstand(State* s)//expected reward after player stands. only dealer is ma
         if(s->dealerval+11>=17)
         {
             if(s->dealerval+11>s->playerval)//player looses
-                rewardace=-1*s->deal;
+                rewardace=-1*deal;
             else if(s->dealerval+11==s->playerval)//PUSH
                 rewardace=0;
             else//player wins
-                rewardace=1*s->deal;   
+                rewardace=1*deal;   
         }
         else
         {
             State* temps;
             statecopy(temps,s);
-            temps->dealerval=temps->dealerval+1;
+            temps->dealerval=temps->dealerval+11;
             temps->dealercardlen=temps->dealercardlen+1;
             temps->dealersofta=temps->dealersofta+1;
-            rewardace=afterstand(temps);
+            rewardace=standreward(temps,deal);
         }
     }
     reward=reward+((1-p)/9)*rewardace;
     return reward;
 }
+
+//reward calculation when decide to hit
+int hitreward(State* s, int deal)
+{
+    //facecard
+    int reward;
+    int rewardface;
+    if(s->playerval+10>21 && s->playersofta==0)//player busts
+        rewardface=-1*deal;
+    else if(s->playerval+10>21 && s->playersofta!=0)//player uses soft A
+    {
+        State* temps;
+        statecopy(temps,s);
+        temps->playersofta=temps->playersofta-1;
+        temps->playercardlen=temps->playercardlen+1;
+        rewardface=freward[statetonumber(temps)][s->dealerval];
+    }
+    else if(s->playerval+10<=21)
+    {
+            State* temps;
+            statecopy(temps,s);
+            temps->playerval=temps->playerval+10;
+            temps->playercardlen=temps->playercardlen+1;
+            rewardface=freward[statetonumber(temps)][s->dealerval];
+    }
+    reward=p*rewardface;
+    //reward to player if non-face card appears(loop-check in sequence)
+    // number cards from 2-9 (ace not included)
+    for(int i=2;i<=9;i++)
+    {
+        int rewardnonface;
+        if(s->playerval+i>21 && s->playersofta==0)//bust
+            rewardnonface=-1*deal;
+        else if(s->playerval+i>21 && s->playersofta!=0)//use soft A
+        {
+            State* temps;
+            statecopy(temps,s);
+            temps->playersofta=temps->playersofta-1;
+            temps->playercardlen=temps->playercardlen+1;
+            temps->playerval=temps->playerval+i-10;
+            rewardnonface=freward[statetonumber(temps)][s->dealerval];
+        }
+        else if(s->playerval+i<=21)
+        {
+            State* temps;
+            statecopy(temps,s);
+            temps->playerval=temps->playerval+i;
+            temps->playercardlen=temps->playercardlen+1;
+            rewardnonface=freward[statetonumber(temps)][s->dealerval];
+        }
+        reward=reward+((1-p)/9)*rewardnonface;
+    }
+    //when ace comes out
+    int rewardace;
+    if(s->playerval+11>21)//soft ACE leading to busting
+    {
+        if(s->playerval+1>21 && s->playersofta==0)//hard ACE leading to busting
+        {
+            rewardace=-1*deal; 
+        }
+        else if(s->playerval+1>21 && s->playersofta!=0)
+        {
+            State* temps;
+            statecopy(temps,s);
+            temps->playerval=temps->playerval+1-10;
+            temps->playercardlen=temps->playercardlen+1;
+            temps->playersofta=temps->playersofta-1;
+            rewardace=freward[statetonumber(temps)][s->dealerval];
+        }
+        else//dealer continues hitting
+        {
+            State* temps;
+            statecopy(temps,s);
+            temps->playerval=temps->playerval+1;
+            temps->playercardlen=temps->playercardlen+1;
+            rewardace=freward[statetonumber(temps)][s->dealerval];
+        }
+    }
+    else
+    {
+        State* temps;
+        statecopy(temps,s);
+        temps->playerval=temps->playerval+1;
+        temps->playercardlen=temps->playercardlen+1;
+        temps->playersofta=temps->playersofta+1;
+        rewardace=freward[statetonumber(temps)][s->dealerval];
+    }
+    reward=reward+((1-p)/9)*rewardace;
+    return reward;
+}
+
+//reward calculation when decide to double
+int doublereward(State* s, int deal)
+{
+    deal=2*deal;
+    //facecard
+    int reward;
+    int rewardface;
+    if(s->playerval+10>21 && s->playersofta==0)//player busts
+        rewardface=-1*deal;
+    else if(s->playerval+10>21 && s->playersofta!=0)//player uses soft A
+    {
+        State* temps;
+        statecopy(temps,s);
+        temps->playersofta=temps->playersofta-1;
+        temps->playercardlen=temps->playercardlen+1;
+        rewardface=standreward(temps,deal);
+    }
+    else if(s->playerval+10<=21)
+    {
+            State* temps;
+            statecopy(temps,s);
+            temps->playerval=temps->playerval+10;
+            temps->playercardlen=temps->playercardlen+1;
+            rewardface=standreward(temps,deal);
+    }
+    reward=p*rewardface;
+    //reward to player if non-face card appears(loop-check in sequence)
+    // number cards from 2-9 (ace not included)
+    for(int i=2;i<=9;i++)
+    {
+        int rewardnonface;
+        if(s->playerval+i>21 && s->playersofta==0)//bust
+            rewardnonface=-1*deal;
+        else if(s->playerval+i>21 && s->playersofta!=0)//use soft A
+        {
+            State* temps;
+            statecopy(temps,s);
+            temps->playersofta=temps->playersofta-1;
+            temps->playercardlen=temps->playercardlen+1;
+            temps->playerval=temps->playerval+i-10;
+            rewardnonface=standreward(temps,deal);
+        }
+        else if(s->playerval+i<=21)
+        {
+            State* temps;
+            statecopy(temps,s);
+            temps->playerval=temps->playerval+i;
+            temps->playercardlen=temps->playercardlen+1;
+            rewardnonface=standreward(temps,deal);
+        }
+        reward=reward+((1-p)/9)*rewardnonface;
+    }
+    //when ace comes out
+    int rewardace;
+    if(s->playerval+11>21)//soft ACE leading to busting
+    {
+        if(s->playerval+1>21 && s->playersofta==0)//hard ACE leading to busting
+        {
+            rewardace=-1*deal; 
+        }
+        else if(s->playerval+1>21 && s->playersofta!=0)
+        {
+            State* temps;
+            statecopy(temps,s);
+            temps->playerval=temps->playerval+1-10;
+            temps->playercardlen=temps->playercardlen+1;
+            temps->playersofta=temps->playersofta-1;
+            rewardace=standreward(temps,deal);
+        }
+        else//dealer continues hitting
+        {
+            State* temps;
+            statecopy(temps,s);
+            temps->playerval=temps->playerval+1;
+            temps->playercardlen=temps->playercardlen+1;
+            rewardace=standreward(temps,deal);
+        }
+    }
+    else
+    {
+        State* temps;
+        statecopy(temps,s);
+        temps->playerval=temps->playerval+1;
+        temps->playercardlen=temps->playercardlen+1;
+        temps->playersofta=temps->playersofta+1;
+        rewardace=standreward(temps,deal);
+    }
+    reward=reward+((1-p)/9)*rewardace;
+    return reward;
+}
+
+//reward when splits
+int splitreward(State* s, int deal)
+{
+    //when pair is non-ace i.e. 2-2,.........10-10
+    if(statetonumber(s)>=24 && statetonumber(s)<=32)
+    {
+        // if a face-card comes up
+        int reward;
+        int rewardface;
+        State* temps;
+        statecopy(temps,s);
+        temps->playerval=temps->playerval/2+10;
+        rewardface=freward[statetonumber(temps)][temps->dealerval];
+        reward=p*rewardface;
+        // if a non-face card comes up
+        for(int i=2;i<=9;i++)
+        {
+            int rewardnonface;
+            statecopy(temps,s);
+            temps->playerval=temps->playerval/2+i;
+            rewardnonface=freward[statetonumber(temps)][temps->dealerval];
+            reward=reward+((1-p)/9)*rewardnonface;
+        }
+        //if ACE comes up
+        int rewardace;
+        statecopy(temps,s);
+        temps->playerval=temps->playerval/2+11;
+        temps->playersofta=temps->playersofta+1;
+        rewardace=freward[statetonumber(temps)][temps->dealerval];
+        reward=reward+((1-p)/9)*rewardace;
+        return 2*reward;//because there are 2 hands like these
+    }
+    else//KAAFI EXCEPTIONS. IN SHORT SIRF STAND KR SKTE HAI.
+    {
+        // if a face-card comes up
+        int reward;
+        int rewardface;
+        State* temps;
+        statecopy(temps,s);
+        temps->playerval=11+10;//-----IS THIS BLACK-JACK??? NO!! IT'S AN EXCEPTION.
+        rewardface=standreward(temps,deal);
+        reward=p*rewardface;
+        // if a non-face card comes up
+        for(int i=2;i<=9;i++)
+        {
+            int rewardnonface;
+            statecopy(temps,s);
+            temps->playerval=11+i;
+            rewardnonface=standreward(temps,deal);
+            reward=reward+((1-p)/9)*rewardnonface;
+        }
+        //if ACE comes up
+        int rewardace;
+        statecopy(temps,s);
+        temps->playerval=11+1;
+        rewardace=standreward(temps,deal);
+        reward=reward+((1-p)/9)*rewardace;
+        return 2*reward;//because there are 2 hands like these
+    }
+} 
